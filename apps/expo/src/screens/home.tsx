8da1204f22332a800755d8207671781393eb0ed6
elevyg <elevyg91@gmail.com>
2022-09-25 18:36:05 -0300

    installing skia

--- apps/expo/src/screens/home.tsx
+++ apps/expo/src/screens/home.tsx
@@ -1,31 +1,38 @@
-import { SafeAreaView, View, Text, TouchableOpacity, TextInput } from "react-native";
-import { FlashList } from "@shopify/flash-list";
-import type { inferProcedureOutput } from "@trpc/server";
-import type { AppRouter } from "@acme/api";
-import { trpc } from "../utils/trpc";
-import React from "react";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+} from "react-native"
+import { FlashList } from "@shopify/flash-list"
+import { Canvas, Circle } from "@shopify/react-native-skia";
+import type { inferProcedureOutput } from "@trpc/server"
+import type { AppRouter } from "@acme/api"
+import { trpc } from "../utils/trpc"
+import React from "react"
 
 const PostCard: React.FC<{
-  post: inferProcedureOutput<AppRouter["post"]["all"]>[number];
+  post: inferProcedureOutput<AppRouter["post"]["all"]>[number]
 }> = ({ post }) => {
   return (
     <View className="p-4 border-2 border-gray-500 rounded-lg">
       <Text className="text-xl font-semibold text-gray-800">{post.title}</Text>
       <Text className="text-gray-600">{post.content}</Text>
     </View>
-  );
-};
+  )
+}
 
 const CreatePost: React.FC = () => {
-  const utils = trpc.useContext();
+  const utils = trpc.useContext()
   const { mutate } = trpc.post.create.useMutation({
     async onSuccess() {
-      await utils.post.all.invalidate();
+      await utils.post.all.invalidate()
     },
-  });
+  })
 
-  const [title, onChangeTitle] = React.useState("");
-  const [content, onChangeContent] = React.useState("");
+  const [title, onChangeTitle] = React.useState("")
+  const [content, onChangeContent] = React.useState("")
 
   return (
     <View className="p-4 border-t-2 border-gray-500 flex flex-col">
@@ -45,21 +52,24 @@ const CreatePost: React.FC = () => {
           mutate({
             title,
             content,
-          });
+          })
         }}
       >
         <Text className="text-white font-semibold">Publish post</Text>
       </TouchableOpacity>
     </View>
-  );
-};
+  )
+}
 
 export const HomeScreen = () => {
-  const postQuery = trpc.post.all.useQuery();
-  const [showPost, setShowPost] = React.useState<string | null>(null);
-
+  const postQuery = trpc.post.all.useQuery()
+  const [showPost, setShowPost] = React.useState<string | null>(null)
+  const r = 128;
   return (
     <SafeAreaView>
+      <Canvas>
+        <Circle cx={r} cy={r} r={r} color="lightblue" />
+      </Canvas>
       <View className="h-full w-full p-4">
         <Text className="text-5xl font-bold mx-auto pb-2">
           Create <Text className="text-indigo-500">T3</Text> Turbo
@@ -90,5 +100,5 @@ export const HomeScreen = () => {
         <CreatePost />
       </View>
     </SafeAreaView>
-  );
-};
+  )
+}
